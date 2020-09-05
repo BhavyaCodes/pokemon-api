eba6b7a770216c99a23efdc8e6f4ccc686532a0e
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-05 23:00:19 +0530

    some bug fixing

--- src/components/BigPokemon.js
+++ src/components/BigPokemon.js
@@ -8,15 +8,31 @@ export default class Pokemon extends Component {
 
 	constructor(props){
 		super(props)
-		this.state = {imageUrl: null, data:{}}
+		this.state = {imageUrl: null, data:{}, isLoading: true, error: false}
 		this.renderTypes = this.renderTypes.bind(this)
 		this.renderAbilities = this.renderAbilities.bind(this)
 		this.renderMoves = this.renderMoves.bind(this)
 	}
 
 	async componentDidMount(){
-		const res = await axios.get(this.props.url)
-		this.setState({data: res.data,imageUrl: res.data.sprites.other.dream_world.front_default})
+		try {
+			const res = await axios.get(this.props.url)
+			this.setState({data: res.data,imageUrl: res.data.sprites.other.dream_world.front_default, isLoading: false, error: false})	
+		} catch (error) {
+			this.setState({error: true, isLoading: false})
+		}
+	}
+
+	async componentDidUpdate(prevProps, prevState){
+		if (prevProps.url !== this.props.url){
+			try {
+				this.setState({isLoading: true})
+				const res = await axios.get(this.props.url)
+				this.setState({data: res.data,imageUrl: res.data.sprites.other.dream_world.front_default, isLoading: false, error: false})	
+			} catch (error) {
+				this.setState({error: true, isLoading: false})
+			}
+		}
 	}
 
 	renderTypes(){
@@ -40,12 +56,50 @@ export default class Pokemon extends Component {
 	}
 
 	render() {
-		if(this.state.imageUrl){
+		if(this.state.isLoading){
+			return (
+				<div class="d-flex justify-content-center">
+					<div class="spinner-border" role="status">
+						<span class="sr-only">Loading...</span>
+					</div>
+				</div>
+		)
+		}
+		if(this.state.error){
+			return(
+				<h3>Error: Unable to find pokemon</h3>
+			)
+		}
 			return(
 				<div className="BigPokemon card mb-3">
 					<div className="row no-gutters">
-						<div className="col-md-4">
-							<img  className="card-img" src={this.state.imageUrl} alt={this.props.name} />
+							<div className="col-md-4">
+								<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+									<ol class="carousel-indicators">
+										<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+										<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+										<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+									</ol>
+									<div class="carousel-inner">
+										<div class="carousel-item active">
+											<img src={this.state.data.sprites.other.dream_world.front_default} class="d-block w-100" alt="..." />
+										</div>
+										<div class="carousel-item">
+											<img src={this.state.data.sprites.front_default} class="d-block w-100" alt="front_default" />
+										</div>
+										<div class="carousel-item">
+											<img src={this.state.data.sprites.back_default} class="d-block w-100" alt="back_default" />
+										</div>
+									</div>
+									<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+										<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+										<span class="sr-only">Previous</span>
+									</a>
+									<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+										<span class="carousel-control-next-icon" aria-hidden="true"></span>
+										<span class="sr-only">Next</span>
+									</a>
+								</div>
 						</div>
 						<div className="col-md-8">
 							<div className="card-body">
@@ -61,6 +115,4 @@ export default class Pokemon extends Component {
 				</div>
 			)
 		}
-		return null
-	}
 }
