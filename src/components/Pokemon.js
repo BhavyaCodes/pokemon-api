823f43be7af6c37f74c1d38c3a591f0407c89ab0
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-05 17:08:50 +0530

    add pokemon type badge

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+import PokemonType from './PokemonType'
 import './Pokemon.css'
 
 export default class Pokemon extends Component {
@@ -8,11 +9,22 @@ export default class Pokemon extends Component {
 	constructor(props){
 		super(props)
 		this.state = {imageUrl: null, data:{}}
+		this.renderTypes = this.renderTypes.bind(this)
 	}
 
 	async componentDidMount(){
 		const res = await axios.get(this.props.url)
 		this.setState({data: res.data,imageUrl: res.data.sprites.other.dream_world.front_default})
+	}
+
+	renderTypes(){
+		if(this.state.data.types){
+			console.log(this.state.data.types)
+			return this.state.data.types.map(type=>{
+				// return (<PokemonType type="xyz" />)
+				return (<PokemonType type={type.type.name}/>)
+			})
+		}
 	}
 
 	render() {
@@ -21,7 +33,7 @@ export default class Pokemon extends Component {
 				{this.state.imageUrl && <img className="card-img-top" src={this.state.imageUrl} alt={this.props.name} />}
 				<div className="card-body">
 					<h5 className="card-title">{this.props.name}</h5>
-					<p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<p className="card-text">{this.renderTypes()}</p>
 				</div>
 			</div>
 		)
--- src/components/PokemonType.js
+++ src/components/PokemonType.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 
 export default class PokemonType extends Component {
+
+	static defaultProps = {type: 'xyz'}
+
 	render() {
 		return (
-			<span class="badge badge-pill badge-info">{this.props.type}</span>
+			<span class="badge badge-pill badge-info mx-1">{this.props.type}</span>
 		)
 	}
 }
