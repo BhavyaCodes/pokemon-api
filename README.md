ea5674cce45ec6475927f2b68b7adca8c182c071
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-06 02:09:03 +0530

    update footer

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Link to App - https://pokemon-alpha.vercel.app/
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 
 import './Footer.css'
+import logo from '../pokemon-logo.png'
 
 export class Footer extends Component {
 	render() {
 		return (
 			<div className="Footer">
-				<p>API used - <a href="https://pokeapi.co/">https://pokeapi.co/</a></p>
-				<p>Source Code - <a href=""></a></p>
+				<div className="Footer-links">
+					<p>API used - <a href="https://pokeapi.co/">https://pokeapi.co/</a></p>
+					<p>Source Code - <a href="https://github.com/Juggernaut9/pokemon-api">https://github.com/Juggernaut9/pokemon-api</a></p>
+				</div>
+				<img src={logo} alt="logo" />
 			</div>
 		)
 	}
